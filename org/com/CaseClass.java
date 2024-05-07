d335849cfffe1bc4b455b543f8f85857473cf871
alexanderperiyasamy <alexander13mech13@gmail.com>
2024-05-07 20:08:49 +0530

    alex

---
+++ org/com/CaseClass.java
@@ -0,0 +1,31 @@
+package org.com;
+
+public class CaseClass {
+	public static void main(String[] args) {
+		
+		int day=4;
+		switch (day) {
+		case 1:
+		
+		System.out.println("sunday");
+		break;
+		case 2:
+			
+		System.out.println("monday");
+		break;
+		case 3:
+			
+		System.out.println("tuesday");
+		break;
+		case 4:
+			
+		System.out.println("wednesday");
+		break;
+		
+		default:
+			break;
+		}
+	
+}
+
+}
